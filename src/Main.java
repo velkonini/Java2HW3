831d66670c344129052ca29fd26cd984bc4c4f79
velkonini <avelkov07@gmail.com>
2020-08-26 20:47:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> teams = new ArrayList<>();
+        teams.add("Lakers");
+        teams.add("Clippers");
+        teams.add("Jazz");
+        teams.add("Nuggets");
+        teams.add("Rockets");
+        teams.add("Blazers");
+        teams.add("Lakers");
+        teams.add("Heat");
+        teams.add("Jazz");
+        teams.add("Raptors");
+        teams.add("Raptors");
+        System.out.println(teams.get(0));
+    }
+}
